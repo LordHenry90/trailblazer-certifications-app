97858f85c06d9e005d76795a605d2c99035bc791
LordHenry90 <notaro.enrico@gmail.com>
2024-07-19 20:08:42 +0200

    Initial commit with SLDS

--- src/components/CertificationDetails.js
+++ src/components/CertificationDetails.js
@@ -6,11 +6,13 @@ const CertificationDetails = ({ certification }) => {
     <div className="certification-details">
       <img src={certification.certificationImageUrl} alt={certification.title} className="slds-image certification-logo-details" />
       <h3 className="slds-text-heading_medium">{certification.title}</h3>
-      <p>{certification.description}</p>
-      <p><strong>Date Completed:</strong> {new Date(certification.dateCompleted).toLocaleDateString()}</p>
-      {certification.dateExpired && <p><strong>Date Expired:</strong> {new Date(certification.dateExpired).toLocaleDateString()}</p>}
-      <p><strong>Status:</strong> {certification.certificationStatus}</p>
-      <a href={certification.certificationUrl} target="_blank" rel="noopener noreferrer">View Certification</a>
+      <p className="slds-text-body_regular">{certification.description}</p>
+      <div className="certification-info">
+        <p><strong>Date Completed:</strong> {new Date(certification.dateCompleted).toLocaleDateString()}</p>
+        {certification.dateExpired && <p><strong>Date Expired:</strong> {new Date(certification.dateExpired).toLocaleDateString()}</p>}
+        <p><strong>Status:</strong> {certification.certificationStatus}</p>
+        <a href={certification.certificationUrl} target="_blank" rel="noopener noreferrer">View Certification</a>
+      </div>
     </div>
   );
 };
