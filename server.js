5916482be36898de4497d0e866be42c0b2399701
LordHenry90 <notaro.enrico@gmail.com>
2024-07-19 19:49:04 +0200

    Initial commit with SLDS

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
 
+// Proxy per le richieste API
+app.use('/api', createProxyMiddleware({
+  target: 'https://go-trailhead-leaderboard-api.up.railway.app',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '',
+  },
+}));
+
+// Servire i file statici della build di React
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function(req, res) {
+// Gestione delle richieste restituendo il file index.html per tutte le altre richieste
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
