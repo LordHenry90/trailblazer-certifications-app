04181726aa369046e8ad53b67c47df55106dedef
LordHenry90 <notaro.enrico@gmail.com>
2024-07-22 08:21:32 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      "stream": require.resolve("stream-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "buffer": require.resolve("buffer/"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "url": require.resolve("url/")
+    }
+  },
+  plugins: [
+    new NodePolyfillPlugin()
+  ]
+};
