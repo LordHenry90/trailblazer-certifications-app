5ec623c48264438627cda2d19d42159fabf3cf93
LordHenry90 <notaro.enrico@gmail.com>
2024-07-22 20:31:32 +0200

    new functions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './App';
 import './index.css';
 
 // Prova entrambi i percorsi per vedere quale funziona
-import 'antd/dist/antd.css';  // Percorso classico
+//import 'antd/dist/antd.css';  // Percorso classico
 // import 'antd/dist/antd.min.css';  // Percorso alternativo
-// import 'antd/dist/reset.css';  // Nuovo percorso (se applicabile)
+import 'antd/dist/reset.css';  // Nuovo percorso (se applicabile)
 
 ReactDOM.render(
   <React.StrictMode>
