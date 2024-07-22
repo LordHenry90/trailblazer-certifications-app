1a6f004600db47ccf8d8724c3504ba98a67dc928
LordHenry90 <notaro.enrico@gmail.com>
2024-07-22 19:45:50 +0200

    new functions

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -9,7 +10,7 @@ app.use(bodyParser.json());
 
 const trailblazerDataPath = path.join(__dirname, '../src/trailblazerData.json');
 
-// Endpoint per ottenere i dati dei Trailblazers
+// API endpoint per ottenere i dati dei Trailblazers
 app.get('/api/trailblazers', (req, res) => {
   fs.readFile(trailblazerDataPath, 'utf8', (err, data) => {
     if (err) {
@@ -19,7 +20,7 @@ app.get('/api/trailblazers', (req, res) => {
   });
 });
 
-// Endpoint per aggiornare i dati dei Trailblazers
+// API endpoint per aggiornare i dati dei Trailblazers
 app.post('/api/trailblazers', (req, res) => {
   const newTrailblazers = req.body;
   fs.writeFile(trailblazerDataPath, JSON.stringify(newTrailblazers, null, 2), (err) => {
@@ -28,6 +29,27 @@ app.post('/api/trailblazers', (req, res) => {
     }
     res.send('Data updated successfully');
   });
+});
+
+// Endpoint per ottenere le certificazioni da un servizio esterno
+app.get('/api/trailblazer/:id/certifications', async (req, res) => {
+  const userId = req.params.id;
+  const apiUrl = `https://go-trailhead-leaderboard-api.up.railway.app/trailblazer/${userId}/certifications`;
+  try {
+    const response = await axios.get(apiUrl);
+    res.send(response.data);
+  } catch (error) {
+    console.error(`Error fetching certifications for ${userId}:`, error);
+    res.status(500).send('Error fetching certifications');
+  }
+});
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../build')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 app.listen(PORT, () => {
